8e79dd56b0eb3aad510f8b51f01fe0a595729e6c
gitraya1400 <159001969+gitraya1400@users.noreply.github.com>
2025-04-28 07:44:09 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,21 @@
-// Import Firebase modules from CDN
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
 
-// Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCi__dxnREoUYqjgklElumTELG2eJp1r_0",
-  authDomain: "howsureday.firebaseapp.com",
-  projectId: "howsureday",
-  storageBucket: "howsureday.firebasestorage.com",
-  messagingSenderId: "1055749509398",
-  appId: "1:1055749509398:web:e0a720bb77401b76b38037",
-  measurementId: "G-P3M6GZ4ZRG"
-};
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
 
-export { app, db };
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app)
