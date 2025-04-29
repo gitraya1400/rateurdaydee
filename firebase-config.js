313b7ac7cdf2e895fd9e0f4bbfb456415a53e340
gitraya1400 <159001969+gitraya1400@users.noreply.github.com>
2025-04-29 16:10:51 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -8,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCi__dxnREoUYqjgklElumTELG2eJp1r_0",
   authDomain: "howsureday.firebaseapp.com",
   projectId: "howsureday",
-  storageBucket: "howsureday.firebasestore.com",
+  storageBucket: "howsureday.appspot.com", // Updated to correct storage bucket
   messagingSenderId: "1055749509398",
   appId: "1:1055749509398:web:e0a720bb77401b76b38037",
   measurementId: "G-P3M6GZ4ZRG",
@@ -19,3 +20,6 @@ const app = initializeApp(firebaseConfig)
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app)
+
+// Initialize Firebase Storage
+export const storage = getStorage(app)
